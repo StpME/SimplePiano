8a412ab2cf662e1c533152327a61a30ed547efa5
StpME <54916725+StpME@users.noreply.github.com>
2019-11-21 10:00:37 -0800

    up to transpose and tempo

--- NotePlayer/src/notePlayer/NotePlayer.java
+++ NotePlayer/src/notePlayer/NotePlayer.java
@@ -7,61 +7,83 @@ public class NotePlayer
 {
     public static void main(String[] args)
     { 
-    	Scanner con=new Scanner(System.in);
-    	System.out.println("Enter note list or list/set instrument");
-    	String input= con.nextLine();
     	
-    	
+    	String input="";
     	while (!(input.equals("quit")))
     	{
-	    	if (input.substring(0,14).equals("set instrument"))
-	    	{
-	    		setI(input);
-	    	}
-	    	else if(input.equals("list instruments"))
-	    	{
-	    		listI(input);
-	    	}
-	    	
-	    	NotePlayer(input);
+    		Scanner con=new Scanner(System.in);
+        	System.out.println("Enter note list or list/set instrument or 'quit' to end");
+        	input= con.nextLine();
+        	if (!(input.equals("quit")))
+        	{
+		    	if (input.contains("_"))
+		    	{
+		    		NotePlayer(input);
+		    	}
+	
+		    	else if(input.equals("list instruments"))
+		    	{
+		    		listI();
+		    	}
+		    	else
+		    	{
+		    		setI(input);
+		    	}
+        	}
     	}
     }
+    
     public static void setI(String input)
     {
     	String set=input;
 		set=set.substring(15);
 		int instrumentNumber=Integer.parseInt(set);
+		setInstrument(instrumentNumber);
     }
     
-    public static void listI(String input)
+    public static void listI()
     {
-    	System.out.print("0: Piano 1        1: Piano 2        2: Piano 3        3: Honky-tonk     4: E.Piano \n"
-"5: E.Piano 2      6: Harpsichord    7: Clav.          8: Celesta        9: Glockenspiel \n"
-"10: Music Box     11: Vibraphone    12: Marimba       13: Xylophone     14: Tubular-bell \n"
-"15: Santur        16: Organ 1       17: Organ 2       18: Organ 3       19: Church Org.1 \n
-"20: Reed Organ    21: Accordion Fr  22: Harmonica     23: Bandoneon     24: Nylon-str.Gt \n
-"25: Steel-str.Gt  26: Jazz Gt.      27: Clean Gt.     28: Muted Gt.     29: Overdrive Gt / n
-"30: DistortionGt  31: Gt.Harmonics  32: Acoustic Bs.  33: Fingered Bs.  34: Picked Bs. \n
-"35: Fretless Bs.  36: Slap Bass 1   37: Slap Bass 2   38: Synth Bass 1  39: Synth Bass 2 \n 
-"40: Violin        41: Viola         42: Cello         43: Contrabass    44: Tremolo Str \n
-"45: PizzicatoStr  46: Harp          47: Timpani       48: Strings       49: Slow Strings \n
-"50: Syn.Strings1  51: Syn.Strings2  52: Choir Aahs    53: Voice Oohs    54: SynVox \n
-"55: OrchestraHit  56: Trumpet       57: Trombone      58: Tuba          59: MutedTrumpet \n
-"60: French Horns  61: Brass 1       62: Synth Brass1  63: Synth Brass2  64: Soprano Sax \n
-"65: Alto Sax      66: Tenor Sax     67: Baritone Sax  68: Oboe          69: English Horn \n
-"70: Bassoon       71: Clarinet      72: Piccolo       73: Flute         74: Recorder \n
-"75: Pan Flute     76: Bottle Blow   77: Shakuhachi    78: Whistle       79: Ocarina \n
-"80: Square Wave   81: Saw Wave      82: Syn.Calliope  83: Chiffer Lead  84: Charang \n
-"85: Solo Vox      86: 5th Saw Wave  87: Bass & Lead   88: Fantasia      89: Warm Pad \n
-"90: Polysynth     91: Space Voice   92: Bowed Glass   93: Metal Pad     94: Halo Pad \n
-"95: Sweep Pad     96: Ice Rain      97: Soundtrack    98: Crystal       99: Atmosphere \n
-"100: Brightness   101: Goblin       102: Echo Drops   103: Star Theme   104: Sitar \n
-"105: Banjo        106: Shamisen     107: Koto         108: Kalimba      109: Bagpipe \n
-"110: Fiddle       111: Shanai       112: Tinkle Bell  113: Agogo        114: Steel Drums \n
-"115: Woodblock    116: Taiko        117: Melo. Tom 1  118: Synth Drum   119: Reverse Cym. \n
-"120: Gt.FretNoise 121: Breath Noise 122: Seashore     123: Bird         124: Telephone 1 \n
-"125: Helicopter   126: Applause     127: Gun Shot \n");
+    	System.out.println("0: Piano 1        1: Piano 2        2: Piano 3        3: Honky-tonk     4: E.Piano");
+    	System.out.println("5: E.Piano 2      6: Harpsichord    7: Clav.          8: Celesta        9: Glockenspiel");
+    	System.out.println("10: Music Box     11: Vibraphone    12: Marimba       13: Xylophone     14: Tubular-bell");
+    	System.out.println("15: Santur        16: Organ 1       17: Organ 2       18: Organ 3       19: Church Org.1");
+    	System.out.println("20: Reed Organ    21: Accordion Fr  22: Harmonica     23: Bandoneon     24: Nylon-str.Gt");
+    	System.out.println("25: Steel-str.Gt  26: Jazz Gt.      27: Clean Gt.     28: Muted Gt.     29: Overdrive Gt");
+    	System.out.println("30: DistortionGt  31: Gt.Harmonics  32: Acoustic Bs.  33: Fingered Bs.  34: Picked Bs.");
+    	System.out.println("35: Fretless Bs.  36: Slap Bass 1   37: Slap Bass 2   38: Synth Bass 1  39: Synth Bass 2");
+    	System.out.println("40: Violin        41: Viola         42: Cello         43: Contrabass    44: Tremolo Str");
+    	System.out.println("45: PizzicatoStr  46: Harp          47: Timpani       48: Strings       49: Slow Strings");
+    	System.out.println("50: Syn.Strings1  51: Syn.Strings2  52: Choir Aahs    53: Voice Oohs    54: SynVox");
+    	System.out.println("55: OrchestraHit  56: Trumpet       57: Trombone      58: Tuba          59: MutedTrumpet");
+    	System.out.println("60: French Horns  61: Brass 1       62: Synth Brass1  63: Synth Brass2  64: Soprano Sax");
+    	System.out.println("65: Alto Sax      66: Tenor Sax     67: Baritone Sax  68: Oboe          69: English Horn");
+    	System.out.println("70: Bassoon       71: Clarinet      72: Piccolo       73: Flute         74: Recorder");
+    	System.out.println("75: Pan Flute     76: Bottle Blow   77: Shakuhachi    78: Whistle       79: Ocarina");
+    	System.out.println("80: Square Wave   81: Saw Wave      82: Syn.Calliope  83: Chiffer Lead  84: Charang");
+    	System.out.println("85: Solo Vox      86: 5th Saw Wave  87: Bass & Lead   88: Fantasia      89: Warm Pad");
+    	System.out.println("90: Polysynth     91: Space Voice   92: Bowed Glass   93: Metal Pad     94: Halo Pad");
+    	System.out.println("95: Sweep Pad     96: Ice Rain      97: Soundtrack    98: Crystal       99: Atmosphere");
+    	System.out.println("100: Brightness   101: Goblin       102: Echo Drops   103: Star Theme   104: Sitar");
+    	System.out.println("105: Banjo        106: Shamisen     107: Koto         108: Kalimba      109: Bagpipe");
+    	System.out.println("110: Fiddle       111: Shanai       112: Tinkle Bell  113: Agogo        114: Steel Drums");
+    	System.out.println("115: Woodblock    116: Taiko        117: Melo. Tom 1  118: Synth Drum   119: Reverse Cym.");
+    	System.out.println("120: Gt.FretNoise 121: Breath Noise 122: Seashore     123: Bird         124: Telephone 1");
+    	System.out.println("125: Helicopter   126: Applause     127: Gun Shot");
 
+    }
+    
+    public static void TandT(String noteinput, int trans, double tempo, String combo)
+    {
+    	trans=Integer.parseInt(noteinput.substring(0,1));
+    	noteinput=noteinput.substring(noteinput.indexOf("_")+1);
+    	tempo=Double.parseDouble(noteinput.substring(0,noteinput.indexOf(",")));
+    	noteinput=noteinput.substring(noteinput.indexOf(",")+1);
+    	
+		System.out.println(noteinput);
+		System.out.println(trans);
+		System.out.println(tempo);
+		combo=noteinput+"$"+trans+"$"+tempo;
+    	
     }
     
     public static void NotePlayer (String input)
@@ -75,6 +97,19 @@ public class NotePlayer
 		int space=noteinput.indexOf(" ");
 		int duration=0;
 		String d="";
+		int trans=0;
+		double tempo=0;
+		String combo="";
+		
+		//transpose and tempo
+		if (noteinput.contains(","))
+		{
+			TandT(noteinput, trans, tempo, combo);
+			noteinput=noteinput.substring(noteinput.indexOf(",")+1);
+		}
+				
+		
+		System.out.println(combo);
 		
 		//for multiple notes
 		if (space !=-1)
@@ -131,20 +166,18 @@ public class NotePlayer
 				space=noteinput.indexOf(" ");
 				if (space!=-1)
 				{
-					d=noteinput.substring(noteinput.indexOf("_")+1,space);
-					duration=Integer.parseInt(d);
+					duration=Integer.parseInt(noteinput.substring(noteinput.indexOf("_")+1,space));
 				}
 				else 
 				{
-					d=noteinput.substring(noteinput.indexOf("_")+1);
-					duration=Integer.parseInt(d);
+					duration=Integer.parseInt(noteinput.substring(noteinput.indexOf("_")+1));
 				}
 				
 				
 				//next note
 				noteinput=noteinput.substring(space+1);				
 				
-				NM(note,accidental,octave,duration);			
+				NM(note,accidental,octave,duration,trans);			
 
 		    		accidental=0;
 		    		octave=0;	
@@ -203,10 +236,9 @@ public class NotePlayer
 					}
 				}
 			}
-			d=noteinput.substring(noteinput.indexOf("_")+1);
-			duration=Integer.parseInt(d);
+			duration=Integer.parseInt(noteinput.substring(noteinput.indexOf("_")+1));
 			
-			NM(note,accidental,octave,duration);			
+			NM(note,accidental,octave,duration,trans);			
 	    	accidental=0;
 	    	octave=0;	
 		}	 
@@ -214,7 +246,7 @@ public class NotePlayer
 
     
     //notenum table and playnote
-    public static void NM(String note, int accidental, int octave, int duration)
+    public static void NM(String note, int accidental, int octave, int duration,int trans)
     {
     	int notenum=0;
     	if (note.equals ("C")) {notenum=60;}
@@ -227,19 +259,19 @@ public class NotePlayer
     	
     		if (accidental==1)
     		{
-    			notenum+=12*octave+1;
+    			notenum+=12*octave+1+trans;
     			playNote(notenum,duration);
     		}
     		else if (accidental==-1)
     		{
     			
     			
-    			notenum+=12*octave-1;
+    			notenum+=12*octave-1+trans;
     			playNote(notenum,duration);
     		}
     		else 
     		{
-    			notenum+=12*octave;
+    			notenum+=12*octave+trans;
     			playNote(notenum,duration);
     		}
     		
@@ -257,6 +289,8 @@ public class NotePlayer
 			noteinput=noteinput.substring(1);
 		}
     }
+    
+
 //-------------------------------------------------------------------//
     /**
      * WARNING!!!  DO NOT MODIFY THIS METHOD.
